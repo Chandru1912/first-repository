3302f9da5962defc97af09a4906ee9d27c4e585e
Chandru1912 <chandrusiva1994@gmail.com>
2026-05-05 15:37:52 +0530

    CloneCheck

--- src/test/java/org/maven/SeleniumClass.java
+++ src/test/java/org/maven/SeleniumClass.java
@@ -14,31 +14,31 @@ public class SeleniumClass
 {
    public static void main(String[] args) throws InterruptedException 
    {
-	      WebDriver driver = new ChromeDriver();
-	      driver.get("https://www.flipkart.com/");
-	      driver.manage().window().maximize();
-	      Actions a = new Actions(driver);
-	      Thread.sleep(3000);
-	      driver.findElement(By.xpath("//span[@class='b3wTlE']")).click();
-	    //  WebElement login = driver.findElement(By.xpath("(//a[contains(@class,'K05FV0')])[1]"));
-	     // a.moveToElement(login).perform();
-	      //driver.findElement(By.xpath("//a[@class='CfNfim' and @title='My Profile']")).click();
-	      JavascriptExecutor js = (JavascriptExecutor)driver;
-	      js.executeScript("window.scrollTo(0,document.body.scrollHeight)");
-	      List<WebElement> li = driver.findElements(By.xpath("//div[@class='nIDtl3']//p"));
-	      for (WebElement p : li)
-	      {
-	    	  System.out.println(p.getText());
-	      }
-	      js.executeScript("window.scrollTo(0,0)");
-	      Thread.sleep(3000);
-	      List<WebElement> elements = driver.findElements(By.xpath("//input[@name='q']"));
-
-	      for (WebElement ele : elements) 
-	      {
-	          if (ele.isDisplayed()) {
-	              ele.sendKeys("watches");	              
-	          }
-	      }
+//	      WebDriver driver = new ChromeDriver();
+//	      driver.get("https://www.flipkart.com/");
+//	      driver.manage().window().maximize();
+//	      Actions a = new Actions(driver);
+//	      Thread.sleep(3000);
+//	      driver.findElement(By.xpath("//span[@class='b3wTlE']")).click();
+//	    //  WebElement login = driver.findElement(By.xpath("(//a[contains(@class,'K05FV0')])[1]"));
+//	     // a.moveToElement(login).perform();
+//	      //driver.findElement(By.xpath("//a[@class='CfNfim' and @title='My Profile']")).click();
+//	      JavascriptExecutor js = (JavascriptExecutor)driver;
+//	      js.executeScript("window.scrollTo(0,document.body.scrollHeight)");
+//	      List<WebElement> li = driver.findElements(By.xpath("//div[@class='nIDtl3']//p"));
+//	      for (WebElement p : li)
+//	      {
+//	    	  System.out.println(p.getText());
+//	      }
+//	      js.executeScript("window.scrollTo(0,0)");
+//	      Thread.sleep(3000);
+//	      List<WebElement> elements = driver.findElements(By.xpath("//input[@name='q']"));
+//
+//	      for (WebElement ele : elements) 
+//	      {
+//	          if (ele.isDisplayed()) {
+//	              ele.sendKeys("watches");	              
+//	          }
+//	      }
    }
 }
